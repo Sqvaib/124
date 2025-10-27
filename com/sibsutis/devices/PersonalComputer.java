1c3f0550d2c3fa469084ac4b8465151fb8a7dabb
Арсений Пьянков <pyankovasenya@gmail.com>
2025-10-28 02:22:20 +0700

    Реализован интерфейс PersonalComputer

---
+++ com/sibsutis/devices/PersonalComputer.java
@@ -0,0 +1,13 @@
+package com.sibsutis.devices;
+
+public class PersonalComputer extends Device{
+    public PersonalComputer(int id,int price){
+        super(id,price,null);
+    }
+    public PersonalComputer(int id, int price,String ip){
+        super(id,price,ip);
+    }
+    public String getDeviceType(){
+        return "PersonalComputer";
+    }
+}
